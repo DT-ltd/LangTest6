a5fdb08ddb8f52f09595f1802d2c0fb2f5c67faa
Rikstar2006 <rikstar2006@yandex.ru>
2015-04-14 13:58:35 +0300

    hhh

--- Unit1.pas
+++ Unit1.pas
@@ -835,7 +835,7 @@ procedure TForm1.Button5Click(Sender: TObject);
 
 procedure TForm1.Button7Click(Sender: TObject);
 begin
-showmessage('Новая фигня');
+showmessage('Нов  ая фигня');
 //и коммент к этой фигне
 end;
 
